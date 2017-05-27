8469a4ffa85c675db1ae4067e90d627ccb7e7a79
Rafael Vettori <rafael.vettori@gmail.com>
2017-05-27 09:03:11 -0300

    adiciona teste na home

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -14,4 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 
 # application = get_wsgi_application()
-application = Cling(get_wsgi_application()) # serve os arquivos staticos antes de ser processado pelo django
+# serve os arquivos staticos antes de ser processado pelo django
+application = Cling(get_wsgi_application())
